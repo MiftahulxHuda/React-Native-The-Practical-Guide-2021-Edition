a3f78bee0f6b4e6deb1ae653d482e1c004c48eb1
Miftahul Huda <huda@tura.group>
2021-02-19 16:22:11 +0700

    Section 4 - 15. Showing an Alert

--- 04_components-styling-layouts_building-real-apps/screens/StartGameScreen.js
+++ 04_components-styling-layouts_building-real-apps/screens/StartGameScreen.js
@@ -5,7 +5,8 @@ import {
     View,
     Button,
     TouchableWithoutFeedback,
-    Keyboard
+    Keyboard,
+    Alert
 } from 'react-native'
 
 import Card from '../components/Card';
@@ -28,7 +29,12 @@ const StartGameScreen = (props) => {
 
     const confirmInputHandler = () => {
         const choseNumber = parseInt(enteredValue);
-        if(choseNumber === NaN || choseNumber <= 0 || choseNumber > 99) {
+        if(isNaN(choseNumber) || choseNumber <= 0 || choseNumber > 99) {
+            Alert.alert(
+                'Invalid number!',
+                'Number has to be a number between 1 and 99.',
+                [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}]
+            );
             return;
         }
         
