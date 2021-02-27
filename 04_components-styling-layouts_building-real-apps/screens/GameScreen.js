a69dbde999f2df257af205e19539a401c98eadad
Miftahul Huda <huda@tura.group>
2021-02-27 09:03:34 +0700

    Section 4 - 36. ScrollView _ Flexbox (Yes, that works)

--- 04_components-styling-layouts_building-real-apps/screens/GameScreen.js
+++ 04_components-styling-layouts_building-real-apps/screens/GameScreen.js
@@ -73,8 +73,8 @@ const GameScreen = (props) => {
                     <Ionicons name="md-add" size={24} color="white" />
                 </MainButton>
             </Card>
-            <View style={styles.list}>
-                <ScrollView>
+            <View style={styles.listContainer}>
+                <ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
                 </ScrollView>
             </View>
@@ -97,16 +97,23 @@ const styles = StyleSheet.create({
         width: 400,
         maxWidth: '90%'
     },
-    list: {
+    listContainer: {
         flex: 1,
         width: '80%'
     },
+    list: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
+    },
     listItem: {
         borderColor: '#ccc',
+        borderWidth: 1,
         padding: 15,
         marginVertical: 10,
         backgroundColor: 'white',
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        width: '60%'
     }
 })
