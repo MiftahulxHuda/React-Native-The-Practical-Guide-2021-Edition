728c29649692c78a9788b431ba0cffa3beea6c95
Miftahul Huda <huda@tura.group>
2021-03-01 06:23:00 +0700

    Section 5 - 10. Rendering Different Layouts

--- 04_components-styling-layouts_building-real-apps/screens/GameScreen.js
+++ 04_components-styling-layouts_building-real-apps/screens/GameScreen.js
@@ -1,11 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { 
-    StyleSheet, 
-    Text, 
-    View, 
-    Alert, 
-    FlatList, 
-    Dimensions 
+import {
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+    FlatList,
+    Dimensions
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -37,10 +37,29 @@ const GameScreen = (props) => {
     const initialGuess = generateRandomBetween(1, 100, props.userChoice)
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
     const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
+    const [availableDeviceWidth, setAvailableDeviceWidth] = useState(
+        Dimensions.get('window').width
+    );
+    const [availableDeviceHeight, setAvailableDeviceHeight] = useState(
+        Dimensions.get('window').height
+    );
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
     const { userChoice, onGameOver } = props;
+
+    useEffect(() => {
+        const updateLayout = () => {
+            setAvailableDeviceWidth(Dimensions.get('window').width);
+            setAvailableDeviceHeight(Dimensions.get('window').height);
+        };
+        
+        Dimensions.addEventListener('change', updateLayout);
+
+        return () => {
+            Dimensions.removeEventListener('change', updateLayout);
+        };
+    });
 
     useEffect(() => {
         if (currentGuess === userChoice) {
@@ -63,15 +82,50 @@ const GameScreen = (props) => {
         } else {
             currentLow.current = currentGuess + 1;
         }
-        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        const nextNumber = generateRandomBetween(
+            currentLow.current, 
+            currentHigh.current, 
+            currentGuess
+        );
         setCurrentGuess(nextNumber);
-        setPastGuesses(currPastGuesses => [nextNumber.toString(), ...currPastGuesses]);
+        setPastGuesses(currPastGuesses => [
+            nextNumber.toString(), 
+            ...currPastGuesses
+        ]);
     };
 
     let listContainerStyle = styles.listContainer;
 
-    if(Dimensions.get('window').width < 350) {
+    if (availableDeviceWidth < 350) {
         listContainerStyle = styles.listContainerBig;
+    }
+
+    if (availableDeviceHeight < 500) {
+        return (
+            <View style={styles.screen}>
+                <Text style={DefaultStyles.title}>Opponent's Guess</Text>
+                <View style={styles.controls}>
+                    <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                        <Ionicons name="md-remove" size={24} color="white" />
+                    </MainButton>
+                    <NumberContainer>{currentGuess}</NumberContainer>
+                    <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                        <Ionicons name="md-add" size={24} color="white" />
+                    </MainButton>
+                </View>
+                <View style={listContainerStyle}>
+                    {/* <ScrollView contentContainerStyle={styles.list}>
+                        {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
+                    </ScrollView> */}
+                    <FlatList
+                        keyExtractor={item => item}
+                        data={pastGuesses}
+                        renderItem={renderListItem.bind(this, pastGuesses.length)}
+                        contentContainerStyle={styles.list}
+                    />
+                </View>
+            </View>
+        );
     }
 
     return (
@@ -115,6 +169,12 @@ const styles = StyleSheet.create({
         marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%'
+    },
+    controls: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '80%'
     },
     listContainer: {
         flex: 1,
