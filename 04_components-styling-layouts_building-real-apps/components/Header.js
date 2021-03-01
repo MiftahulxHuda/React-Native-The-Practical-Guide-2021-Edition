7e56c72fbd5c1ddd3520fb5afa71e6d32c066f34
Miftahul Huda <huda@tura.group>
2021-03-01 11:44:50 +0700

    Section 5 - 15. Introducing the Platform API

--- 04_components-styling-layouts_building-real-apps/components/Header.js
+++ 04_components-styling-layouts_building-real-apps/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Platform } from 'react-native'
 
 import Colors from '../constants/colors';
 import TitleText from './TitleText';
@@ -19,8 +19,13 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Colors.primary,
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+    },
+    title: {
+        color: Platform.OS === 'ios' ? Colors.primary : 'white'
     }
 });
